3265addbdf99346912a713e4ca28857f63af0944
Naasi-LF <2737492311@qq.com>
2024-12-10 20:01:33 +0800

    master

---
+++ backend/src/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import { connectDB } from './lib/db.js';
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
+
+app.listen(PORT, () => {
+    
+    console.log(`
+                                                 ,----,                                                 
+                    ,--,                       ,/   .\`|                         ,-.----.    ,-.----.    
+  ,----..         ,--.'|    ,---,            ,\`   .'  :            ,---,        \\    /  \\   \\    /  \\   
+ /   /   \\     ,--,  | :   '  .' \\         ;    ;     /           '  .' \\       |   :    \\  |   :    \\  
+|   :     : ,---.'|  : '  /  ;    '.     .'___,/    ,'           /  ;    '.     |   |  .\\ : |   |  .\\ : 
+.   |  ;. / |   | : _' | :  :       \\    |    :     |           :  :       \\    .   :  |: | .   :  |: | 
+.   ; /--\`  :   : |.'  | :  |   /\\   \\   ;    |.';  ;           :  |   /\\   \\   |   |   \\ : |   |   \\ : 
+;   | ;     |   ' '  ; : |  :  ' ;.   :  \`----'  |  |           |  :  ' ;.   :  |   : .   / |   : .   / 
+|   : |     '   |  .'. | |  |  ;/  \\   \\     '   :  ;           |  |  ;/  \\   \\ ;   | |\`-'  ;   | |\`-'  
+.   | '___  |   | :  | ' '  :  | \\  \\ ,'     |   |  '           '  :  | \\  \\ ,' |   | ;     |   | ;     
+'   ; : .'| '   : |  : ; |  |  '  '--'       '   :  |           |  |  '  '--'   :   ' |     :   ' |     
+'   | '/  : |   | '  ,/  |  :  :             ;   |.'            |  :  :         :   : :     :   : :     
+|   :    /  ;   : ;--'   |  | ,'             '---'              |  | ,'         |   | :     |   | :     
+ \\   \\ .'   |   ,/       \`--''                                  \`--''           \`---'.|     \`---'.|     
+  \`---\`     '---'                                                                 \`---\`       \`---\`     
+                                                                                                         
+    `);
+    console.log(`server is running on port ${PORT}`);
+    connectDB();
+});
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true } // 自动生成 createdAt 和 updatedAt 时间戳
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
